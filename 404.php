ca74cb61acd71e9de34286b32609b9501af4f54e
Matt <matt@mspacecreative.com>
2020-11-20 18:10:28 -0400

    c

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <div class="contentWrap">
 	<div class="innerContainer w1080 topPadding4">
-		<h1 class="postTitle"><?php esc_html_e( 'Page not found' ); ?></h1>
+		<h1 class="postTitle"><?php echo esc_html_e( 'Page not found' ); ?></h1>
 		<h2>
-		 	<a href="<?php esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?' ); ?></a>
+		 	<a href="<?php echo esc_url( home_url() ); ?>"><?php echo esc_html_e( 'Return home?' ); ?></a>
 		</h2>
 	</div>
 </div>
