1a2fb70db81400823dc0255a5f99c1d941670898
Matt <matt@mspacecreative.com>
2020-11-23 14:40:04 -0400

    c

--- templates/loops/loop-posts-sidebar.php
+++ templates/loops/loop-posts-sidebar.php
@@ -9,38 +9,29 @@ $arr_posts = new WP_Query( $args ); ?>
 <?php if ( $arr_posts->have_posts() ) : ?>
 
 <div class="row gutterSpaceWide cardGrid">
-		
-	<?php while ( $arr_posts->have_posts() ) : $arr_posts->the_post(); ?>
+			
+		<?php while ( $arr_posts->have_posts() ) : $arr_posts->the_post(); ?>
 			
 		<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-			<div class="row">
-				
-				<div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				
-					<div class="colInner cardContainer whiteBg">
+			<div class="colInner cardContainer whiteBg">
+
+				<!-- post title -->
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<h2><?php the_title(); ?></h2>
+				</a>
+				<!-- /post title -->
 		
-						<!-- post title -->
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-								<h2><?php the_title(); ?></h2>
-							</a>
-							<!-- /post title -->
-					
-							<!-- post details -->
-							<h4 class="date">
-								<?php echo the_time( 'F j, Y' ); ?><?php esc_html_e(' &nbsp;| &nbsp;'); ?><?php the_category(', '); ?>
-							</h4>
-							<!-- /post details -->
-					
-							<p><?php osp_custom_excerpt('osp_index'); ?></p>
-						</div>
-					</div>
-					
-				</div>
-				
+				<!-- post details -->
+				<h4 class="date">
+					<?php echo the_time( 'F j, Y' ); ?><?php esc_html_e(' &nbsp;| &nbsp;'); ?><?php the_category(', '); ?>
+				</h4>
+				<!-- /post details -->
+		
+				<a class="readMore" href="<?php the_permalink(); ?>"><?php esc_html_e('Read more', 'osp'); ?></a>
 			</div>
 		</div>
-			
-	<?php endwhile; ?>
+		
+		<?php endwhile; ?>
 	
 </div>	
 <?php endif; 
