2a59f2cd598e96110c1addfd6fabe728929133f0
Matt <matt@mspacecreative.com>
2020-11-23 12:34:54 -0400

    c

--- templates/blocks/includes/content-cta-button-light.php
+++ templates/blocks/includes/content-cta-button-light.php
@@ -2,15 +2,15 @@
 <?php while( have_rows('content_cta_button') ): the_row(); ?>
 	<?php if ( get_sub_field('content_internal_link') ): ?>
 	<ul class="actions">
-		<li><a href="<?php the_sub_field('content_internal_link'); ?>" class="light button blue"><?php the_sub_field('content_button_label'); ?></a></li>
+		<li><a href="<?php the_sub_field('content_internal_link'); ?>" class="dark button blue"><?php the_sub_field('content_button_label'); ?></a></li>
 	</ul>
 	<?php elseif ( get_sub_field('content_external_link') ): ?>
 	<ul class="actions">
-		<li><a href="<?php the_sub_field('content_external_link'); ?>" class="light button blue" target="_blank"><?php the_sub_field('content_button_label'); ?></a></li>
+		<li><a href="<?php the_sub_field('content_external_link'); ?>" class="dark button blue" target="_blank"><?php the_sub_field('content_button_label'); ?></a></li>
 	</ul>
 	<?php elseif ( get_sub_field('content_anchor_link') ): ?>
 	<ul class="actions">
-		<li><a href="#<?php the_sub_field('content_anchor_link'); ?>" class="light button blue"><?php the_sub_field('content_button_label'); ?></a></li>
+		<li><a href="#<?php the_sub_field('content_anchor_link'); ?>" class="dark button blue"><?php the_sub_field('content_button_label'); ?></a></li>
 	</ul>
 	<?php endif; ?>
 <?php endwhile; ?>
