3aa3f153e6b482c2a75c64386bf5bb3c0192e8e6
Matt <matt@mspacecreative.com>
2020-11-23 08:32:34 -0400

    c

--- single.php
+++ single.php
@@ -21,7 +21,7 @@
 				<h1 class="postTitle" style="margin-bottom: 20px;"><?php the_title(); ?></h1>
 				<?php 
 				$category = get_the_category( $post->ID ); ?>
-				<h4 class="date bottomMargin50"><?php esc_html_e('Posted on '); ?><?php echo date( 'F j, Y' ); ?><?php esc_html_(' &nbsp;| &nbsp;'); ?><?php echo $category ?></h4>
+				<h4 class="date bottomMargin50"><?php esc_html_e('Posted on '); ?><?php echo date( 'F j, Y' ); ?><?php esc_html_e(' &nbsp;| &nbsp;'); ?><?php echo $category ?></h4>
 			</div>
 		</div>
 
