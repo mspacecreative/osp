6b1752ad5243eba3edc2bb4d8fd1de3c1bc71f9a
Matt <matt@mspacecreative.com>
2020-11-23 08:22:50 -0400

    c

--- single.php
+++ single.php
@@ -19,7 +19,7 @@
 		<div class="row">
 			<div class="col col-lg-8 col-md-8 col-sm-12 col-xs-12">
 				<h1 class="postTitle bottomMargin50"><?php the_title(); ?></h1>
-				<span class="date"><?php esc_html_e('Posted on '); ?><?php echo date( 'F j, Y' ); ?></span>
+				<h4 class="date bottomMargin50"><?php esc_html_e('Posted on '); ?><?php echo date( 'F j, Y' ); ?></h4>
 			</div>
 		</div>
 
