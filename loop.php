01031176c3ad98f5f0569805c5f4fc62be6c135e
Matt <matt@mspacecreative.com>
2020-11-23 09:33:27 -0400

    c

--- loop.php
+++ loop.php
@@ -7,7 +7,7 @@
 		
 			<?php while (have_posts()) : the_post(); ?>
 			
-			<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12 cardContainer whiteBg">
+			<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 			
 				<div class="row">
 					
@@ -25,7 +25,7 @@
 					<div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<?php endif; ?>
 					
-						<div class="colInner">
+						<div class="colInner cardContainer whiteBg">
 			
 							<!-- post title -->
 							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
