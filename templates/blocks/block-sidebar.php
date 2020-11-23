2d87f257c3b1b588e47a67f12ed4737a3fa19eba
Matt <matt@mspacecreative.com>
2020-11-23 13:21:59 -0400

    c

--- templates/blocks/block-sidebar.php
+++ templates/blocks/block-sidebar.php
@@ -13,14 +13,16 @@ switch ( $sidebar ) {
 		<?php 
 		$intro = get_field('intro_content');
 		$outro = get_field('outro_content');
+		$showintro = get_field('add_content_before_feed');
+		$showoutro = get_field('add_content_after_feed');
 		
-		if ( $intro ) {
+		if ( $intro && $showintro ) {
 			echo $intro;
 		}
 		
 		if(!function_exists('dynamic_sidebar') || !dynamic_sidebar( $result ));
 		
-		if ( $outro ) {
+		if ( $outro && $showoutro ) {
 			echo $outro;
 		} ?>
 	</div>
