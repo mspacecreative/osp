11c2044b1a17d84c6519025fa8793b0c4c69fa4d
Matt <matt@mspacecreative.com>
2020-11-27 10:09:51 -0400

    c

--- loop-list.php
+++ loop-list.php
@@ -14,6 +14,7 @@
 				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 					<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
 				</a>
+				<?php endif; ?>
 				<!-- /post thumbnail -->
 				
 				<!-- post title -->
