9df7363e12dccda02dcafe7d82562ed51c2cd432
Matt <matt@mspacecreative.com>
2020-11-27 11:20:12 -0400

    c

--- page.php
+++ page.php
@@ -14,16 +14,18 @@ if ( have_posts() ) {
 	 	<div class="contentWrap">';
 	 		if ( $titlewidth === '1080' ) {
 	 		echo '
-	 		<div class="innerContainer w1080 section4">';
+	 		<section>
+	 			<div class="innerContainer w1080 section4">';
 	 		} elseif ( $titlewidth === '850' ) {
 	 		echo '
-	 		<div class="innerContainer w850 section4">';
+	 			<div class="innerContainer w850 section4">';
 	 		} else {
 		 	echo '
-	 		<div class="innerContainer section4">';
+	 			<div class="innerContainer section4">';
 		 	}
 			echo '<h1 class="postTitle">' . $title . '</h1>
-			</div>';
+				</div>
+			</section>';
 			echo the_content(); 
 		echo '
 		</div>';
