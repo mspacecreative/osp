0b19a435fa367b6e54c2625f7809c0e600fc3d82
Matt <matt@mspacecreative.com>
2020-11-27 12:02:15 -0400

    c

--- page-sidebar.php
+++ page-sidebar.php
@@ -22,29 +22,33 @@ switch ( $titlewidth ) {
 <?php 
 $title = get_the_title();
 echo '
-	<div class="innerContainer section4 ' . $width . '">
-	 		
-	 	<h1 class="postTitle">' . $title . '</h1>
-			
-	</div>';
+	<section>
+		<div class="innerContainer section4 ' . $width . '">
+		 		
+		 	<h1 class="postTitle">' . $title . '</h1>
+				
+		</div>
+	</section>';
 
 if ( have_posts() ) {
  	while ( have_posts() ) { 
 	the_post();
 	 	
 	echo '
-	<div class="innerContainer">
-		<div class="row gutterSpaceWide">
-			<div class="col col-lg-9 col-md-9 col-sm-12 col-xs-12">'; 
-				echo the_content();
-			echo '
-			</div>
-			<div class="col col-lg-3 col-md-3 col-sm-12 col-xs-12">';
-				echo get_sidebar('page');
-			echo '
+	<section>
+		<div class="innerContainer">
+			<div class="row gutterSpaceWide">
+				<div class="col col-lg-9 col-md-9 col-sm-12 col-xs-12">'; 
+					echo the_content();
+				echo '
+				</div>
+				<div class="col col-lg-3 col-md-3 col-sm-12 col-xs-12">';
+					echo get_sidebar('page');
+				echo '
+				</div>
 			</div>
 		</div>
-	</div>';
+	</section>';
 	}
 } ?>
 
