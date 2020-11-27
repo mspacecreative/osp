d49376b4e49dcf79748ec4d55f6dead691ac2741
Matt <matt@mspacecreative.com>
2020-11-27 12:14:17 -0400

    c

--- page.php
+++ page.php
@@ -6,9 +6,6 @@ $titlewidth = get_field('title_row_width');
 if ( have_posts() ) {
  	while ( have_posts() ) { 
 	 	the_post();
-	 	if ( $titlewidth === 'narrow' ) {
-		 		
-		 	}
 	 	$title = get_the_title();
 	 	echo '
 	 	<div class="contentWrap">';
@@ -18,9 +15,11 @@ if ( have_posts() ) {
 	 			<div class="innerContainer w1080 section4">';
 	 		} elseif ( $titlewidth === '850' ) {
 	 		echo '
+	 		<section>
 	 			<div class="innerContainer w850 section4">';
 	 		} else {
 		 	echo '
+		 	<section>
 	 			<div class="innerContainer section4">';
 		 	}
 			echo '<h1 class="postTitle">' . $title . '</h1>
