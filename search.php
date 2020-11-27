43d9481ece9c86f2659d9f04513111fbcd20e5a2
Matt <matt@mspacecreative.com>
2020-11-27 10:21:40 -0400

    c

--- search.php
+++ search.php
@@ -1,13 +1,16 @@
 <?php get_header(); ?>
 
 <div class="contentWrap">';
+	
 	<div class="innerContainer w1080 section4">
-	 	
 	 	<h1><?php echo sprintf( __( '%s Search Results for ', 'osp' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
-	 	<?php get_template_part( 'loop-list' ); ?>
-	 	<?php get_template_part( 'pagination' ); ?>
-	 	
 	</div>
+	
+	<div class="innerContainer w1080">
+		<?php get_template_part( 'loop-list' ); ?>
+	 	<?php get_template_part( 'pagination' ); ?>
+	</div>
+	
 </div>
 
 <?php get_footer(); ?>
