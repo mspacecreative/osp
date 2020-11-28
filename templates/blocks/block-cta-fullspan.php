b0c0534cb90a0626e19dd51d122bb2b93f9ff813
Matt <matt@mspacecreative.com>
2020-11-28 16:16:29 -0400

    c

--- templates/blocks/block-cta-fullspan.php
+++ templates/blocks/block-cta-fullspan.php
@@ -12,9 +12,41 @@ $showsocial = get_field('show_social_media_icons');
 				the_row();
 				$radarposition = get_sub_field('position');
 				if ( $radarposition === 'left' ) {
-					echo '<div class="radarLeft"></div>';
+					echo '
+					<div class="radarLeft">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						 viewBox="0 0 250.5 501" style="enable-background:new 0 0 250.5 501;" xml:space="preserve">
+							<path class="st st0" d="M250.5,0.5c-138.1,0-250,111.9-250,250s111.9,250,250,250" />
+							
+							<path class="st st1" d="M250.5,31.9c-120.7,0-218.6,97.9-218.6,218.6s97.9,218.6,218.6,218.6" />
+							
+							<path class="st st2" d="M250.5,65.3c-102.3,0-185.2,82.9-185.2,185.2s82.9,185.2,185.2,185.2" />
+							
+							<path class="st st3" d="M250.5,101.2c-82.5,0-149.3,66.9-149.3,149.3S168,399.8,250.5,399.8" />
+							
+							<path class="st st4" d="M250.5,147.7c-56.8,0-102.8,46-102.8,102.8c0,56.8,46,102.8,102.8,102.8" />
+							
+							<path class="st st5" d="M250.5,190.7c-33,0-59.8,26.8-59.8,59.8c0,33,26.8,59.8,59.8,59.8V190.7z" />
+						</svg>
+					</div>';
 				} elseif ( $radarposition === 'right' ) {
-					echo '<div class="radarRight"></div>';
+					echo '
+					<div class="radarRight">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						 viewBox="0 0 250.5 501" style="enable-background:new 0 0 250.5 501;" xml:space="preserve">
+							<path class="st st0" d="M250.5,0.5c-138.1,0-250,111.9-250,250s111.9,250,250,250" />
+							
+							<path class="st st1" d="M250.5,31.9c-120.7,0-218.6,97.9-218.6,218.6s97.9,218.6,218.6,218.6" />
+							
+							<path class="st st2" d="M250.5,65.3c-102.3,0-185.2,82.9-185.2,185.2s82.9,185.2,185.2,185.2" />
+							
+							<path class="st st3" d="M250.5,101.2c-82.5,0-149.3,66.9-149.3,149.3S168,399.8,250.5,399.8" />
+							
+							<path class="st st4" d="M250.5,147.7c-56.8,0-102.8,46-102.8,102.8c0,56.8,46,102.8,102.8,102.8" />
+							
+							<path class="st st5" d="M250.5,190.7c-33,0-59.8,26.8-59.8,59.8c0,33,26.8,59.8,59.8,59.8V190.7z" />
+						</svg>
+					</div>';
 				}
 			}
 		}
