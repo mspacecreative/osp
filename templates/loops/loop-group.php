95b91ae9f0615b70ad957106d315fa9efe2039c5
Matt <matt@mspacecreative.com>
2020-12-01 13:47:51 -0400

    c

--- templates/loops/loop-group.php
+++ templates/loops/loop-group.php
@@ -25,10 +25,11 @@ if ( $loop->have_posts() ) : ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post();
 			$featuredimg = get_the_post_thumbnail( get_the_ID(), 'card-feature' );
 			$title = get_the_title();
-			$excerpt = get_the_excerpt(); ?>
+			$excerpt = get_the_excerpt();
+			$permalink = get_the_permalink( get_the_ID() ); ?>
 			<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 				<div class="cardContainer">
-					<a href="<?php get_the_permalink(); ?>">
+					<a href="<?php echo $permalink ?>">
 						<?php echo $featuredimg ?>
 					</a>
 					<div class="cardContent">
