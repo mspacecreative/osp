f3300503ca5ca26b5b446a6d3586d7130f876d50
Matt <matt@mspacecreative.com>
2020-12-01 12:45:28 -0400

    c

--- taxonomy-group-researchers-and-students.php
+++ taxonomy-group-researchers-and-students.php
@@ -1,45 +1,15 @@
-<?php 
-$args = array(
-	'post_type' => 'audience',
-	'posts_per_page'=> -1,
-	'tax_query' => array (
-		array(
-			'taxonomy' => 'group',
-			'field' => 'slug',
-			'terms' => 'researchers-and-students',
-		)
-	)
-); ?>
+<?php get_header(); ?>
 
-<section>
-	 <div class="innerContainer w1080 section4">
-	 	<h1 class="postTitle"><?php esc_html_e('All audiences'); ?></h1>
-	 </div>
-</section>
+<div class="contentWrap">
+	
+	<section>
+		 <div class="innerContainer w1080 section4">
+		 	<h1 class="postTitle"><?php echo $term->name ?></h1>
+		 </div>
+	</section>
+	
+	<?php get_template_part('templates/loops/loop-researchers-and-students'); ?>
+	
+</div>
 
-<?php 
-$loop = new WP_Query( $args );
-if ( $loop->have_posts() ) : ?>
-<section class="section">
-	<div class="offsetBg">
-		<div class="innerContainer">
-			<div class="container">
-				<div class="row">
-				<?php while ( $loop->have_posts() ) : $loop->the_post();
-				$featuredimg = get_the_post_thumbnail( 'medium' );
-				$title = get_the_title();
-				$excerpt = get_the_excerpt(); ?>
-				<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12 cardContainer">
-					<?php 
-					echo $featuredimg . '
-					<div class="cardContent">
-						<h2>' . $title . '</h2>' . '
-						<p>' . $excerpt . '</p>
-					</div>';
-				endwhile; ?>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<?php endif;  wp_reset_query(); ?>
+<?php get_footer(); ?>
