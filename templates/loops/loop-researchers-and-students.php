a20b62df8e1f2c73843cfa8051c0ff2dbec2d23d
Matt <matt@mspacecreative.com>
2020-12-01 12:55:13 -0400

    c

--- templates/loops/loop-researchers-and-students.php
+++ templates/loops/loop-researchers-and-students.php
@@ -1,34 +1,41 @@
-<div class="contentWrap">
+<?php 
+$args = array(
+	'post_type' => 'audience',
+	'posts_per_page'=> -1,
+	'tax_query' => array (
+		array(
+			'taxonomy' => 'group',
+			'field' => 'slug',
+			'terms' => 'researchers-and-students',
+		)
+	)
+);
+	
+$loop = new WP_Query( $args );
+if ( $loop->have_posts() ) : ?>
 
-	<section>
-		 <div class="innerContainer w1080 section4">
-		 	<h1 class="postTitle"><?php esc_html_e('All audiences'); ?></h1>
-		 </div>
-	</section>
+<section class="section">
 	
-	<?php 
-	$args = array(
-		'post_type' => 'audience',
-		'posts_per_page'=> -1,
-	);
+	<div class="offsetBg"></div>
 	
-	$loop = new WP_Query( $args );
-	if ( $loop->have_posts() ) : ?>
-	<section class="section">
-		<div class="offsetBg"></div>
-		<div class="innerContainer w1080">
-			<div class="row gutterSpaceWide feedGrid">
+	<div class="innerContainer w1080">
+		<div class="row gutterSpaceWide feedGrid">
+			
 			<?php while ( $loop->have_posts() ) : $loop->the_post();
 			$featuredimg = get_the_post_thumbnail( get_the_ID(), 'card-feature' );
 			$title = get_the_title();
 			$excerpt = get_the_excerpt(); ?>
 			<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 				<div class="cardContainer">
-					<?php 
-					echo '<a href="' . get_the_permalink() . '">' . $featuredimg . '</a>
+					<a href="<?php get_the_permalink(); ?>">
+						<?php echo $featuredimg ?>
+					</a>
 					<div class="cardContent">
 						<div class="colInner">
-							<a style="color: #333;" href="' . get_the_permalink() . '"><h3>' . $title . '</h3></a>'; 
+							<a style="color: #333;" href="<?php get_the_permalink(); ?>">
+								<h3><?php echo $title ?></h3>
+							</a>
+							<?php 
 							if ( $excerpt ) {
 								echo '<p>' . $excerpt . '</p>';
 							} else {
@@ -42,7 +49,8 @@
 			<?php endwhile; ?>
 			
 		</div>
-	</section>
-	<?php endif;  wp_reset_query(); ?>
+	</div>
+	
+</section>
 
-</div>
+<?php endif;  wp_reset_query(); ?>
