3288c02c3fbc1518f74c77d9ca576352e12d47c4
Matt <matt@mspacecreative.com>
2020-12-02 11:35:58 -0400

    c

--- category.php
+++ category.php
@@ -2,26 +2,36 @@
 
 <div id="main" class="contentWrap">
 
-	<?php
-	$title = get_the_title();
-	$titlewidth = get_field('title_row_width');
-	
-	if ( $titlewidth === '1080' ) : ?>
-	<div class="innerContainer w1080 section4">
-	
-	<?php elseif ( $titlewidth === '850' ) : ?>
-	<div class="innerContainer w850 section4">
-	
-	<?php else : ?>
-	<div class="innerContainer section4 w1080">
-	<?php endif; ?>
-	
-		<h1 class="postTitle"><span class="grey"><?php esc_html_e( 'Category ', 'osp' ); ?></span><?php single_cat_title(); ?></h1>
+	<section>
+
+		<?php
+		$title = get_the_title();
+		$titlewidth = get_field('title_row_width');
 		
-		<?php get_template_part( 'templates/loops/loop-category' );
-		get_template_part( 'pagination' ); ?>
+		if ( $titlewidth === '1080' ) : ?>
+		<div class="innerContainer w1080 section4">
 		
-	</div>
+		<?php elseif ( $titlewidth === '850' ) : ?>
+		<div class="innerContainer w850 section4">
+		
+		<?php else : ?>
+		<div class="innerContainer section4 w1080">
+		<?php endif; ?>
+		
+			<h1 class="postTitle"><span class="grey"><?php esc_html_e( 'Category ', 'osp' ); ?></span><?php single_cat_title(); ?></h1>
+			
+		</div>
+		
+	</section>
+	
+	<section>
+	
+		<div class="innerContainer bottomPadding w1080">
+			<?php get_template_part( 'templates/loops/loop-category' );
+			get_template_part( 'pagination' ); ?>
+		</div>
+		
+	</section>
 </div>
 
 <?php get_footer(); ?>
