737ad3a06b434cda3777d7aefa9ffcdb1f8bf3bb
Matt <matt@mspacecreative.com>
2020-12-02 14:09:47 -0400

    c

--- loop-list.php
+++ loop-list.php
@@ -8,14 +8,28 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="searchResult">
 			
-			<!-- post thumbnail -->
 			<?php if ( has_post_thumbnail() ) : // Check if thumbnail exists. ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
-			</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+					<!-- post thumbnail -->
+					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+						<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
+					</a>
+					<!-- /post thumbnail -->
+				</div>
 				
+				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+					<!-- post title -->
+					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+						<h2><?php the_title(); ?></h2>
+					</a>
+					<!-- /post title -->
+					
+					<p><?php osp_custom_excerpt('osp_index'); ?></p>
+				</div>
+			</div>
+			
+			<?php else : ?>
 			<!-- post title -->
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 				<h2><?php the_title(); ?></h2>
@@ -23,6 +37,7 @@
 			<!-- /post title -->
 					
 			<p><?php osp_custom_excerpt('osp_index'); ?></p>
+			<?php endif; ?>
 				
 		</div>
 	</article>
