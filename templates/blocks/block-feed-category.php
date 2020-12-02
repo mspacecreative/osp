986fca04f4234f028241b8a0741162c5fa213489
Matt <matt@mspacecreative.com>
2020-12-02 16:27:20 -0400

    c

--- templates/blocks/block-feed-category.php
+++ templates/blocks/block-feed-category.php
@@ -1,9 +1,39 @@
 <?php 
 $heading = get_field('heading');
 $category = get_field('group');
-$cannedimg = get_field('image_selection', get_the_ID() );
 
-switch ( $cannedimg ) {
+if ( $category ) {
+
+	$args = array(
+		'posts_per_page'=> 3,
+		'category_name' => $category->slug,
+	);
+
+	$loop = new WP_Query( $args );
+	if ( $loop->have_posts() ) : ?>
+	
+	<section class="section">
+		<div class="offsetBg"></div>
+		
+		<div class="innerContainer w1080 clearfix">
+			<?php 
+			if ( $heading ) {
+				echo '<h2 class="light">' . $heading . '</h2>';
+			} else {
+				echo '<h2 class="light">' . __('What&#8217;s new') . '</h2>';
+			} ?>
+			
+			<div class="row gutterSpaceWide feedGrid">
+				
+				<?php while ( $loop->have_posts() ) : $loop->the_post();
+				$featuredimg = get_the_post_thumbnail( get_the_ID(), 'card-feature' );
+				$title = get_the_title();
+				$permalink = get_the_permalink( get_the_ID() );
+				$excerpt = get_the_excerpt();
+				$url = get_field('url', get_the_ID());
+			$cannedimg = get_field('image_selection');
+			
+			switch ( $cannedimg ) {
 				case 'pan-atl-partner':
 					$img = '
 					<div class="cannedImgContainer">
@@ -394,37 +424,7 @@ switch ( $cannedimg ) {
 				    		</svg>
 						</div>
 					</div>';
-			}
-
-if ( $category ) {
-
-	$args = array(
-		'posts_per_page'=> 3,
-		'category_name' => $category->slug,
-	);
-
-	$loop = new WP_Query( $args );
-	if ( $loop->have_posts() ) : ?>
-	
-	<section class="section">
-		<div class="offsetBg"></div>
-		
-		<div class="innerContainer w1080 clearfix">
-			<?php 
-			if ( $heading ) {
-				echo '<h2 class="light">' . $heading . '</h2>';
-			} else {
-				echo '<h2 class="light">' . __('What&#8217;s new') . '</h2>';
 			} ?>
-			
-			<div class="row gutterSpaceWide feedGrid">
-				
-				<?php while ( $loop->have_posts() ) : $loop->the_post();
-				$featuredimg = get_the_post_thumbnail( get_the_ID(), 'card-feature' );
-				$title = get_the_title();
-				$permalink = get_the_permalink( get_the_ID() );
-				$excerpt = get_the_excerpt();
-				$url = get_field('url', get_the_ID()); ?>
 				<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 					<div class="cardContainer">
 						<?php 
