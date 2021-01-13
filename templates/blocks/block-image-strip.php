2782b5c3f1b9c3ad0cbe372f2e5815e60f6a3907
Matt <matt@mspacecreative.com>
2021-01-13 10:36:36 -0400

    c

--- templates/blocks/block-image-strip.php
+++ templates/blocks/block-image-strip.php
@@ -3,15 +3,19 @@ $images = get_field('gallery');
 $size = 'photo-strip';
 
 if ( $images ): ?>
-<section class="section3 container">
-	<div class="row imageStripContainer">
-		<?php foreach( $images as $image_id ): ?>
-		<div class="col col-lg-4 col-md-4 col-sm-4 col-xs-4">
-			<div class="imageStripInner">
-				<?php echo wp_get_attachment_image( $image_id, $size ); ?>
+<section class="section3 container" style="max-width: 100%;">
+	<div class="stripOuter">
+		<div class="stripInner">
+			<div class="row imageStripContainer">
+				<?php foreach( $images as $image_id ): ?>
+				<div class="col col-lg-4 col-md-4 col-sm-4 col-xs-4">
+					<div class="imageStripInner">
+						<?php echo wp_get_attachment_image( $image_id, $size ); ?>
+					</div>
+				</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
-		<?php endforeach; ?>
 	</div>
 </section>
 <?php endif; ?>
