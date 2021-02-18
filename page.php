fa37067d15eb6b60f67dc93fc589680396d977db
Matt <matt@mspacecreative.com>
2021-02-18 12:39:17 -0400

    c

--- page.php
+++ page.php
@@ -2,34 +2,35 @@
 get_header();
 
 $titlewidth = get_field('title_row_width');
+$title = get_the_title(); ?>
+<div id="main" class="contentWrap">
+	 		
+	<section>
+	 			
+	 	<?php if ( $titlewidth === '1080' ) : ?>
+	 	<div class="innerContainer w1080 section4">
+	 			
+	 	<?php elseif ( $titlewidth === '850' ) : ?>
+	 	<div class="innerContainer w850 section4">
+	 			
+	 	<?php else : ?>
+	 	<div class="innerContainer section4">';
+	 	<?php endif; ?>
+	 			
+			<h1 class="postTitle"><?php echo $title ?></h1>
+					
+		</div>
+			
+	</section>
+			
+	<?php
+	if ( have_posts() ) {
+	 	while ( have_posts() ) {
+			the_post();
+			the_content();
+		}
+	} ?>
+			
+</div>
 
-if ( have_posts() ) {
- 	while ( have_posts() ) { 
-	 	the_post();
-	 	$title = get_the_title();
-	 	echo '
-	 	<div id="main" class="contentWrap">';
-	 		if ( $titlewidth === '1080' ) {
-	 		echo '
-	 		<section>
-	 			<div class="innerContainer w1080 section4">';
-	 		} elseif ( $titlewidth === '850' ) {
-	 		echo '
-	 		<section>
-	 			<div class="innerContainer w850 section4">';
-	 		} else {
-		 	echo '
-		 	<section>
-	 			<div class="innerContainer section4">';
-		 	}
-			echo '<h1 class="postTitle">' . $title . '</h1>
-				</div>
-			</section>';
-			echo the_content(); 
-		echo '
-		</div>';
-	}
-}
-
-get_footer();
-?>
+<?php get_footer();
