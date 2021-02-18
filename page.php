6fc12f88e454c46f5bdca360a90c8570fa4af989
Matt <matt@mspacecreative.com>
2021-02-18 12:41:19 -0400

    c

--- page.php
+++ page.php
@@ -7,14 +7,14 @@ $title = get_the_title(); ?>
 	 		
 	<section>
 	 			
-	 	<?php if ( $titlewidth === '1080' ) : ?>
+	 	<?php if ( $titlewidth === '1080' ): ?>
 	 	<div class="innerContainer w1080 section4">
 	 			
-	 	<?php elseif ( $titlewidth === '850' ) : ?>
+	 	<?php elseif ( $titlewidth === '850' ): ?>
 	 	<div class="innerContainer w850 section4">
 	 			
 	 	<?php else : ?>
-	 	<div class="innerContainer section4">';
+	 	<div class="innerContainer section4">
 	 	<?php endif; ?>
 	 			
 			<h1 class="postTitle"><?php echo $title ?></h1>
