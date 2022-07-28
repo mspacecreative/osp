7ec6aa9a8c92c184c61ec89e6c95d5b4e461dd69
Matt Cyr <matt@mspacecreative.com>
2022-07-28 08:24:30 -0300

    Added section width option to archive block

--- templates/blocks/block-archive.php
+++ templates/blocks/block-archive.php
@@ -2,6 +2,21 @@
 $format = get_field('display_format');
 $bgimg = get_field('background_image') ? ' style="background-image: url(' . get_field('background_image') . ');"' : '';
 $textcolor = (get_field('text_colour') == 'light') ? ' light' : '';
+$sectionwidth = get_field('section_width');
+switch ($sectionwidth) {
+	case '1280':
+		$sectionwidth = '';
+		break;
+	case '1080':
+		$sectionwidth = ' w1080';
+		break;
+	case '850':
+		$sectionwidth = ' w1850';
+		break;
+	default:
+		$sectionwidth = '';
+		break;
+}
 
 if ( $format === 'grid' ) {
 	
