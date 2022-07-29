e4d3c5d08905040ca70fd0f9a823e34fca9d5ccf
Matt Cyr <matt@mspacecreative.com>
2022-07-29 12:45:13 -0300

    Feature companies (#19)
    * Flexbox
    
    Adjusted margin and padding on grid boxes to accommodate overflow on container.
    
    * Testimonial
    
    Fixed conditions on author, title and company
    
    * Removed unused CSS folder
    
    and fixed error in archive block
    
    * Testimonials
    
    Added external URL field to code
    
    * Testimonial
    
    Fixed missing postid declaration
    
    * Testimonials
    
    Fix to grid layout if there is a full width testimonial with logo

--- templates/blocks/includes/block-loop-grid.php
+++ templates/blocks/includes/block-loop-grid.php
@@ -47,9 +47,11 @@ switch ( $cols ) {
 		break;
 				
 	default :
-		$flex = 'class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 one_col"';
-		$fullcoltop = 'class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 quote_content"';
-		$fullcolbottom = 'class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 object_fit no_padding_left"';
+		$flex = ' class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 one_col"';
+		$fullcoltopwithlogo = ' class="col col-lg-8 col-md-8 col-sm-12 col-xs-12 quote_content"';
+		$fullcolbottomwithlogo = ' class="col col-lg-4 col-md-4 col-sm-12 col-xs-12 object_fit no_padding_left"';
+		$fullcoltop = ' class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 quote_content"';
+		$fullcolbottom = ' class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 object_fit no_padding_left"';
 }
 
 if ( $blockanchor ): ?>
@@ -80,7 +82,7 @@ if ( $blockanchor ): ?>
 			$logo = get_field('logo', $post->ID);
 			$size = 'medium'; ?>
 			
-			<div <?php echo $flex ?>>
+			<div<?php echo $flex ?>>
 			
 				<div class="container testimonialContainer">
 						
@@ -90,18 +92,28 @@ if ( $blockanchor ): ?>
 					<div class="row no_image_logo">	
 					<?php endif; ?>
 							
-						<div <?php echo $fullcoltop ?>>
+						<?php if ( !empty($featuredimg) || !empty($logo) ): ?>
+						<div<?php echo $fullcoltopwithlogo; ?>>
+						<?php else : ?>
+						<div<?php echo $fullcoltop ?>>
+						<?php endif; ?>
+
 							<div class="testimonialContainerInner">
 								<?php
 								echo '<div class="quoteContent">' . $quote . '</div>';
 								include 'testimonial-author.php';
 								?>
 							</div>
+
 						</div>
 							
 						<?php 
 						if ( !empty( $featuredimg ) || !empty( $logo ) ): ?>
-						<div <?php echo $fullcolbottom ?>>
+						<div<?php echo $fullcolbottomwithlogo; ?>>
+						<?php else : ?>
+						<div<?php echo $fullcolbottom ?>>
+						<?php endif; ?>
+
 							<?php 
 							if ( $featuredimg ):
 							echo wp_get_attachment_image( $featuredimg, $size );
@@ -110,6 +122,7 @@ if ( $blockanchor ): ?>
 								<?php echo wp_get_attachment_image( $logo, $size ); ?>
 							</div>
 							<?php endif; ?>
+							
 						</div> 
 						<?php 
 						endif; ?>
