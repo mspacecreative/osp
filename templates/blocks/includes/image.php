44e568acb64f17a78728b5010c2326dcf63e209a
Matt <matt@mspacecreative.com>
2022-08-18 10:11:37 -0300

    Still trying to fix error

--- templates/blocks/includes/image.php
+++ templates/blocks/includes/image.php
@@ -1,5 +1,5 @@
 <?php
 $staticimg = get_sub_field('static_image');
 if( !empty( $img ) ): ?>
-<img class="full-width" src="<?php echo esc_url($staticimg['url']); ?>" alt="<?php echo esc_attr($staticimg['alt']); ?>">
+<img class="full-width" src="<?php echo $staticimg['url']; ?>" alt="<?php echo esc_attr($staticimg['alt']); ?>">
 <?php endif; ?>
