6bcd75a9f14923d30b3b1de9109c19c7e13cf610
Matt <matt@mspacecreative.com>
2022-08-26 09:40:29 -0300

    More CSS changes

--- templates/blocks/includes/content-cta-button-dark.php
+++ templates/blocks/includes/content-cta-button-dark.php
@@ -10,7 +10,7 @@
 	</ul>
 	<?php elseif ( get_sub_field('content_anchor_link') ): ?>
 	<ul class="actions<?php if ( $align ): echo ' '; echo $align; endif; ?>">
-		<li><a href="#<?php the_sub_field('content_anchor_link'); ?>" class="btn light><?php the_sub_field('content_button_label'); ?></a></li>
+		<li><a href="#<?php the_sub_field('content_anchor_link'); ?>" class="btn light"><?php the_sub_field('content_button_label'); ?></a></li>
 	</ul>
 	<?php endif; ?>
 <?php endwhile; ?>
