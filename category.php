cc41ddf3d6015bd450e1f8cf27773a4359247810
Matt <matt@mspacecreative.com>
2022-08-29 11:31:26 -0300

    Changes to category template

--- category.php
+++ category.php
@@ -5,39 +5,25 @@ include 'templates/blocks/includes/horizontal-alignment.php'; ?>
 <div id="main" class="contentWrap">
 
 	<section>
-
 		<?php
 		$title = get_the_title();
-		$titlewidth = get_field('title_row_width');
-		
-		if ( $titlewidth === '1080' ) : ?>
-		<div class="innerContainer w1080 section4">
-		
-		<?php elseif ( $titlewidth === '850' ) : ?>
-		<div class="innerContainer w800 section4">
-		
-		<?php else : ?>
-		<div class="innerContainer section4 w1080">
-		<?php endif; ?>
-		
-			<h1 class="postTitle"><span class="grey"><?php esc_html_e( 'Category ', 'osp' ); ?></span><?php single_cat_title(); ?></h1>
-			
+		$textalignment = (get_field('text_alignment') == 'center') ? ' text-align-center' : ''; ?>
+		<div class="innerContainer section4<?php echo $sectionwidth ?>">
+			<h1 class="postTitle<?php echo $textalignment ?>"><span class="grey"><?php esc_html_e( 'Category ', 'osp' ); ?></span><?php single_cat_title(); ?></h1>
 		</div>
-		
 	</section>
 	
 	<section>
-	
 		<div class="innerContainer bottomPadding">
-			<div class="container<?php echo $sectionwidth, $horizontalalignment ?>">
+			<div class="container<?php echo $sectionwidth ?>">
 				<?php 
 				get_template_part( 'templates/loops/loop-category' );
 				get_template_part( 'pagination' ); 
 				?>
 			</div>
 		</div>
-		
 	</section>
+
 </div>
 
 <?php get_footer(); ?>
