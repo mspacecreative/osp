008fda254ea5a8ea6323f07c1c84134e17b6b831
Matt Cyr <matt@mspacecreative.com>
2022-09-23 08:03:30 -0300

    c

--- templates/blocks/block-team.php
+++ templates/blocks/block-team.php
@@ -147,8 +147,8 @@ echo
             '<div class="bio-overlay">
                 <div class="bio-overlay--inner">
                     <div class="bio-content-table-cell" style="position: relative;">
-                        <span class="mc-close-window" style="background-image: url(' . get_template_directory_uri() . '/assets/img/icons/close.png); background-size: 100%;"></span>
-                        <div>    
+                        <div style="position: relative;">
+                            <span class="mc-close-window" style="background-image: url(' . get_template_directory_uri() . '/assets/img/icons/close.png); background-size: 100%;"></span>  
                             ' . $content . '
                         </div>
                     </div>
