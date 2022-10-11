70acf06f4e5b0c21054db02ff72bbb915d85a0c6
Matt <matt@mspacecreative.com>
2022-10-11 12:37:20 -0300

    Fixed email now button bug

--- templates/blocks/block-team.php
+++ templates/blocks/block-team.php
@@ -71,7 +71,7 @@ echo
                 $content = $item->post_content ? $item->post_content : '';
                 $featimg = get_the_post_thumbnail_url($item->ID, 'medium-square');
                 $readbio = '<a href="#" data-id="' . $item->ID . '" class="btn blue read-bio">' . __('Read bio') . '</a>'; 
-                $email = get_field('email_address', $item->ID) ? '<a href="mailto:' . get_field('email_address', $item->ID) . '" class="btn blue read-bio">' . __('Email now') . '</a>' : '';
+                $email = get_field('email_address', $item->ID) ? '<a href="mailto:' . get_field('email_address', $item->ID) . '" class="btn blue">' . __('Email now') . '</a>' : '';
                 $bluebutton = $content ? $readbio : $email;
                 //$externallink = get_field('url', $item->ID);
                 $linklabel = get_field('button_label', $item->ID) ? get_field('button_label', $item->ID) : __('Read bio');
