ad7d52b4034f1031af7a310437e8c00b796ed470
Matt <matt@mspacecreative.com>
2022-10-19 08:52:10 -0300

    Fixed PHP error

--- templates/blocks/block-team.php
+++ templates/blocks/block-team.php
@@ -165,7 +165,7 @@ echo
                             <div class="col button-top-margin col-lg-12 col-md-12 col-sm-12 col-xs-12">';
                         } else {
                             echo
-                            '<div class="col button-top-margin col-lg-9 col-md-9 col-sm-8 col-xs-12">;
+                            '<div class="col button-top-margin col-lg-9 col-md-9 col-sm-8 col-xs-12">';
                         }
                                 echo $title;
                             
